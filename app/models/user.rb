b0e616743cac066355000b29035c74ca9afb439f
melikegundogdu <gundogdu.mel@gmail.com>
2022-03-08 17:06:08 +0800

    modelsandassosiationsaredone

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :events # events I created
+  has_many :messages
+  has_many :user_events
+  has_many :joined_events, through: :user_events, foreign_key: "user_id", source: :event # events I joined
+  has_many :my_events_chatrooms, through: :events, foreign_key: "user_id", source: :chatroom # Chatroom that User acts as owner
+  has_many :joined_chatrooms, through: :joined_events, foreign_key: "user_id", source: :chatroom # Chatroom that User joined an event
+
+  def chatrooms
+    my_events_chatrooms + joined_chatrooms
+  end
 end
