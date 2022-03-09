f8e71d624f764df1cfa953484d840cf2631e65e5
melikegundogdu <gundogdu.mel@gmail.com>
2022-03-09 12:11:51 +0800

    devise registaration and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :events do
+    resources :user_events, only: [:create, :destroy]
+  end
+  resources :chatrooms, only: [:index, :show] do
+    resources :messages, only: [:create, :destroy]
+  end
 end
