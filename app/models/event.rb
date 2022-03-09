1f492fd29cf66367181fab5209d15b200c4411bb
melikegundogdu <gundogdu.mel@gmail.com>
2022-03-09 14:18:51 +0800

    sports as string

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,9 @@
 class Event < ApplicationRecord
+  SPORTS = ['Surfing', 'Yoga', 'Swimming', 'Boxing', 'Hiking']
   belongs_to :user
   has_many :user_events
   has_many :users, through: :user_events
   has_one :chatroom
+
+  validates :sport, inclusion: {in: SPORTS}
 end
