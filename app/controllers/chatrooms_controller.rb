90fa8842bb39a742625313f404e293465764de0e
melikegundogdu <gundogdu.mel@gmail.com>
2022-03-10 16:40:10 +0800

    messageandchatbox

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,2 +1,9 @@
 class ChatroomsController < ApplicationController
+  def index
+    @chatrooms = Chatroom.includes(:messages)
+  end
+  def show
+    @chatroom = Chatroom.includes(:messages).find(params[:id])
+    @message = Message.new
+  end
 end
