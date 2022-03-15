8bf79661b87c4953c5c2cdcfcae1c7d26f89e744
Joana Moufat <joana.moufat@gmail.com>
2022-03-15 10:58:12 +0800

    go back btn fixed and content card message changed

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,9 +1,10 @@
 class ChatroomsController < ApplicationController
   def index
-    @chatrooms = current_user.chatrooms
+    @chatrooms = current_user.chatrooms.uniq
   end
 
   def show
+    @chatrooms = current_user.chatrooms
     @chatroom = Chatroom.includes(:messages).find(params[:id])
     @message = Message.new
   end
